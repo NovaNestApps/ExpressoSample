c80a57b068a70e2efcbe8467c2c3c1bb2d7b4ad1
Trupti Gawde <143222562+truptig12@users.noreply.github.com>
2025-06-11 22:16:51 +0100

    Update SwitchActions.kt

--- mobile/core/uitesting/espresso/actions/SwitchActions.kt
+++ mobile/core/uitesting/espresso/actions/SwitchActions.kt
@@ -3,12 +3,23 @@ package com.mobile.core.uitesting.espresso.actions
 import android.view.View
 import androidx.test.espresso.ViewAction
 
+/**
+ * Contains a reusable action for setting the checked state of a Switch view.
+ */
 object SwitchActions {
+
+    /**
+     * Sets the checked state of a Switch (e.g., ON/OFF).
+     *
+     * @param expectedState Desired checked state to set (`true` or `false`)
+     */
     fun setSwitchToExpectedState(expectedState: Boolean): ViewAction = object : ViewAction {
         override fun getDescription() = "Set Switch to expected checked state"
-        override fun getConstraints() = isAssignableFrom(android.widget.Switch::class.java)
-        override fun perform(uiController: androidx.test.espresso.UiController, view: View) {
-            val switchView = view as? android.widget.Switch
+
+        override fun getConstraints() = isAssignableFrom(Switch::class.java)
+
+        override fun perform(uiController: UiController, view: View) {
+            val switchView = view as? Switch
             if (switchView != null && switchView.isChecked != expectedState) {
                 switchView.isChecked = expectedState
             }
