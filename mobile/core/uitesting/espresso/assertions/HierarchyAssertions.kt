eb9e8f18279d14e3cbc8a3f3795ad36ba6e5e3fe
Trupti Gawde <143222562+truptig12@users.noreply.github.com>
2025-06-11 22:32:15 +0100

    Update HierarchyAssertions.kt

--- mobile/core/uitesting/espresso/assertions/HierarchyAssertions.kt
+++ mobile/core/uitesting/espresso/assertions/HierarchyAssertions.kt
@@ -5,32 +5,27 @@ import androidx.annotation.IdRes
 import org.hamcrest.Matcher
 
 /**
- * Hierarchy-based assertions for descendants and siblings.
+ * Assertions for validating UI hierarchy relationships like descendants and siblings.
  */
 interface HierarchyAssertions {
-    /** Checks if a view has a descendant matching the given matcher. */
+
+    /**
+     * Asserts that a view with [viewId] has a descendant matching [descendantMatcher].
+     */
     fun hasDescendant(@IdRes viewId: Int, descendantMatcher: Matcher<View>)
+
+    /**
+     * Asserts that a view matching [viewMatcher] has a descendant matching [descendantMatcher].
+     */
     fun hasDescendant(viewMatcher: Matcher<View>, descendantMatcher: Matcher<View>)
 
-    /** Checks if a view has a sibling matching the given matcher. */
+    /**
+     * Asserts that a view with [viewId] has a sibling matching [siblingMatcher].
+     */
     fun hasSibling(@IdRes viewId: Int, siblingMatcher: Matcher<View>)
+
+    /**
+     * Asserts that a view matching [viewMatcher] has a sibling matching [siblingMatcher].
+     */
     fun hasSibling(viewMatcher: Matcher<View>, siblingMatcher: Matcher<View>)
-}
-
-object DefaultHierarchyAssertions : HierarchyAssertions {
-    override fun hasDescendant(@IdRes viewId: Int, descendantMatcher: Matcher<View>) =
-        androidx.test.espresso.Espresso.onView(androidx.test.espresso.matcher.ViewMatchers.withId(viewId))
-            .check(androidx.test.espresso.assertion.ViewAssertions.matches(androidx.test.espresso.matcher.ViewMatchers.hasDescendant(descendantMatcher)))
-
-    override fun hasDescendant(viewMatcher: Matcher<View>, descendantMatcher: Matcher<View>) =
-        androidx.test.espresso.Espresso.onView(viewMatcher)
-            .check(androidx.test.espresso.assertion.ViewAssertions.matches(androidx.test.espresso.matcher.ViewMatchers.hasDescendant(descendantMatcher)))
-
-    override fun hasSibling(@IdRes viewId: Int, siblingMatcher: Matcher<View>) =
-        androidx.test.espresso.Espresso.onView(androidx.test.espresso.matcher.ViewMatchers.withId(viewId))
-            .check(androidx.test.espresso.assertion.ViewAssertions.matches(androidx.test.espresso.matcher.ViewMatchers.hasSibling(siblingMatcher)))
-
-    override fun hasSibling(viewMatcher: Matcher<View>, siblingMatcher: Matcher<View>) =
-        androidx.test.espresso.Espresso.onView(viewMatcher)
-            .check(androidx.test.espresso.assertion.ViewAssertions.matches(androidx.test.espresso.matcher.ViewMatchers.hasSibling(siblingMatcher)))
 }
