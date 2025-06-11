275ab0c89949cfd0f05238b085c89dac94744ed3
Trupti Gawde <143222562+truptig12@users.noreply.github.com>
2025-06-11 22:43:39 +0100

    Update CustomAssertions.kt

--- mobile/core/uitesting/espresso/assertions/CustomAssertions.kt
+++ mobile/core/uitesting/espresso/assertions/CustomAssertions.kt
@@ -1,12 +1,16 @@
 /**
- * Custom view assertions that go beyond standard Espresso matchers.
+ * Custom assertions that go beyond default Espresso checks.
+ * Useful for checking parent view states and explicitly verifying non-display.
  */
 class CustomAssertions {
+
     companion object {
 
         /**
-         * Asserts that the view is NOT displayed.
-         * Fails if view is present in the hierarchy and is visible.
+         * Asserts that the view is not displayed on the screen.
+         *
+         * @return A [ViewAssertion] that throws an error if the view is displayed.
+         * @throws AssertionError if the view is present and displayed.
          */
         fun isNotDisplayed(): ViewAssertion = ViewAssertion { view, _ ->
             if (view != null && isDisplayed().matches(view)) {
@@ -15,8 +19,10 @@ class CustomAssertions {
         }
 
         /**
-         * Asserts that the parent of the view is enabled.
-         * Fails if parent is null or not enabled.
+         * Asserts that the parent of the current view is enabled.
+         *
+         * @return A [ViewAssertion] that throws an error if the parent view is disabled.
+         * @throws AssertionError if the parent is disabled.
          */
         fun isParentEnabled(): ViewAssertion = ViewAssertion { view, _ ->
             val parentView = view.parent
@@ -26,8 +32,10 @@ class CustomAssertions {
         }
 
         /**
-         * Asserts that the parent of the view is disabled.
-         * Fails if parent is enabled.
+         * Asserts that the parent of the current view is disabled.
+         *
+         * @return A [ViewAssertion] that throws an error if the parent view is enabled.
+         * @throws AssertionError if the parent is enabled.
          */
         fun isParentDisabled(): ViewAssertion = ViewAssertion { view, _ ->
             val parentView = view.parent
